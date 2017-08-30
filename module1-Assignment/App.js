fad292283ec2697b9f46101a0b802c735f8eb7d2
sandeep <sandeep@gmail.com>
2017-08-30 22:46:42 +0530

    App.js modified

--- module1-Assignment/App.js
+++ module1-Assignment/App.js
@@ -1,48 +1,57 @@
 (function(){
-	'use strict';
+  'use strict';
 
-	angular.module('MyApp',[])
+  angular.module('MyApp',[])
 
-	.controller('MyController',function($scope){
+  .controller('MyController',function($scope){
         
 
         $scope.check   = function(){
-        	var msg;
+          var msg;
             var list       = $scope.menu;
             $scope.custom  = {};
             //console.log(list);return false;
             if(list === undefined || list === null){
 
-		          msg                       =  "Please enter data first";
-		          $scope.custom.style       =  {"color":"red"};
-		          $scope.custom.borderStyle =  {"border-color":"red"};
-		      
-		     }else{
+              msg                       =  "Please enter data first";
+              $scope.custom.style       =  {"color":"red"};
+              $scope.custom.borderStyle =  {"border-color":"red"};
+          
+         }else{
 
-            	  list    = list.split(',');
-            	  
-            	  list    = list.filter(function(value){
+                list    = list.split(',');
+                
+                list    = list.filter(function(value){
                      if(value != null || value != undefined){
                         return value;
                      }
-            	  });
+                });
                   
-                  //console.log(list);return false;
-		          if(list.length <= 3){
-		           	 msg  = "Enjoy!";
-		          }
+                  console.log(list.length);
+                  //return false;
+              if(list.length <= 3 && list.length > 0){
+                 msg  = "Enjoy!";
+                 $scope.custom.style = {"color":"green"};
+                     $scope.custom.borderStyle = {"border-color":"green"};
+              }
                   else if(list.length > 3){
-		             msg  = "Too much!";
-		          }
+                 msg  = "Too much!";
+                 $scope.custom.style = {"color":"green"};
+                     $scope.custom.borderStyle = {"border-color":"green"};
+              }else{
+                 msg                       =  "Please enter data first";
+                 $scope.custom.style       =  {"color":"red"};
+                 $scope.custom.borderStyle =  {"border-color":"red"};
+              }
+              
 
-                  $scope.custom.style = {"color":"green"};
-                  $scope.custom.borderStyle = {"border-color":"green"};
+                  
             }
 
             $scope.message = msg;
 
         };
 
-	});
+  });
 
 })();
